8d42353a49b28c0de5404eea9cbd48ffad736ca5
Julius Pupalaigis <julius.pupalaigis@ktu.edu>
2016-03-12 20:19:15 +0200

    Optimized sql query to get an array beacons

--- BtMatuoklis/app/src/main/java/com/example/btmatuoklis/classes/ChartHelper.java
+++ BtMatuoklis/app/src/main/java/com/example/btmatuoklis/classes/ChartHelper.java
@@ -2,7 +2,6 @@ package com.example.btmatuoklis.classes;
 
 import android.app.Activity;
 import android.content.Context;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
