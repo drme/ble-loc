7628950704dc6072fcd36065ece1c4d2cebe2d95
Julius Pupalaigis <julius.pupalaigis@ktu.edu>
2016-04-03 20:02:16 +0300

    Test

--- BtMatuoklis/app/src/main/java/com/example/btmatuoklis/classes/RoomDetector.java
+++ BtMatuoklis/app/src/main/java/com/example/btmatuoklis/classes/RoomDetector.java
@@ -20,6 +20,7 @@ public class RoomDetector {
         settings = MainActivity.settings;
     }
 
+    
     public String getRoomName(RoomsArray created, RoomsArray enviroment){
         if (!created.getArray().isEmpty()) {
             coeff = new ArrayList<Byte>();
